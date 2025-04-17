c4d29568c4f5b66ff726e65c3bd025c4f28b0a29
장효찬 <jang@hyochan.site>
2025-04-17 14:15:43 +0900

    카테고리 링크 수정 - 카테고리 표시 방식 변경: 각 카테고리를 링크로 감싸서 표시 - 링크 클릭 시 카테고리 경로로 이동 가능하게 수정 - 코드 가독성 향상: `each` 블록 사용하여 반복 처리

--- src/routes/[...slug]/+page.svelte
+++ src/routes/[...slug]/+page.svelte
@@ -108,7 +108,12 @@
     <!-- 카테고리 트리형 표시 (제목 위에 작은 텍스트로) -->
     {#if data.data?.category && data.data.category.length > 0}
         <p class="text-xs text-gray-500 px-4 pt-4 mt-2">
-            {data.data.category.join(" > ")}
+            {#each data.data.category as tag, i}
+                {@html (() => {
+                    let path = data.data.category.slice(0, i + 1).map(encodeURIComponent).join('/').toLowerCase();
+                    return `<a href=\"/category/${path}\" class=\"hover:underline text-blue-600\">${tag}</a>${i < data.data.category.length - 1 ? ' > ' : ''}`;
+                })()}
+            {/each}
         </p>
     {/if}
 
