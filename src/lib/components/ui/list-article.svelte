59b19859a37dba64a4ef989b32eb985337e9ca08
Henry Jang <jang@hyochan.site>
2025-05-04 19:05:11 +0900

    리스트 아티클 컴포넌트 수정
    - Svelte 스크립트에 TypeScript 지원 추가
    - 날짜 표시 로직 개선 및 현재 시간 기준으로 표시
    - 아티클 설명 추가 및 날짜 형식 변경
    - CSS 클래스 수정으로 레이아웃 조정

--- src/lib/components/ui/list-article.svelte
+++ src/lib/components/ui/list-article.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
     import {
         Banana,
         Beer,
@@ -18,6 +18,7 @@
         Ham,
         CalendarRange,
     } from "lucide-svelte";
+    import { onMount } from "svelte";
 
     const icons = [
         Banana,
@@ -43,12 +44,36 @@
     const Icon = icons[parseInt(post.uid.slice(0, 1), 16)];
 
     let date = new Date(post.date);
+    let now = new Date();
     let formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`;
     let formattedTime = `${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}`;
+
+    let displayDate = $state(formattedDate + " " + formattedTime);
+
+    function getDisplayDate(dateString: string) {
+        let date = new Date(dateString);
+        let now = new Date();
+        let diff = Math.floor((now.getTime() - date.getTime()) / 1000);
+        let formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`;
+        let formattedTime = `${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}`;
+        if (diff < 60) {
+            return `${diff}초 전`;
+        } else if (diff < 3600) {
+            return `${Math.floor(diff / 60)}분 전`;
+        } else if (diff < 36 * 3600) {
+            return `${Math.floor(diff / 3600)}시간 전`;
+        } else {
+            return `${formattedDate} ${formattedTime}`;
+        }
+    }
+
+    onMount(() => {
+        displayDate = getDisplayDate(post.date);
+    });
 </script>
 
 <a class="flex text-left bg-white rounded-lg" href={`/${post.permalink}`}>
-    <section class="w-40 flex items-center justify-center shrink-0 grow-0">
+    <section class="w-48 flex items-center justify-center shrink-0 grow-0">
         {#if post.thumbnail}
             <img src={post.thumbnail} alt={post.withOutSeries} />
         {:else}
@@ -58,12 +83,19 @@
             </span>
         {/if}
     </section>
-    <section class="flex flex-col p-4">
+    <section class="flex flex-col p-4 min-w-0">
         <span class="text-xs block">{post.category.join(" > ")}</span>
         {#if post.series}
-            <span class="text-xs block">{post.series} #{post.seriesIndex}</span>
+            <span class="text-base block font-semibold text-gray-500">
+                {post.series} #{post.seriesIndex}
+            </span>
         {/if}
-        <h2 class="text-xl font-bold block">{post.withOutSeries}</h2>
-        <span class="text-xs block">{formattedDate} {formattedTime}</span>
+        <h2 class="text-xl font-bold block truncate w-full">
+            {post.withOutSeries}
+        </h2>
+        <h3 class="text-sm text-gray-500">
+            {post.description}
+        </h3>
+        <span class="text-xs block">{displayDate}</span>
     </section>
 </a>
