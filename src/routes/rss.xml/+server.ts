c6c191e04c1fff9846f7ba43af389264c2a69e32
장효찬 <jang@hyochan.site>
2025-05-09 10:50:05 +0900

    서버 로직 개선 및 RSS, 사이트맵 추가
    - `src/routes/rss.xml/+server.ts` 파일 수정: RSS 피드 생성 로직 추가
    - `src/routes/sitemap.xml/+server.ts` 파일 수정: 사이트맵 생성 로직 추가
    - 포스트 정렬 및 XML 형식으로 응답 반환 로직 구현

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -1,3 +1,35 @@
+import { getPosts } from "$lib/utils.server";
+
+export const prerender = true;
+
 export const GET = async () => {
-    return new Response("A");
+    const posts = getPosts();
+    const sortedPosts = posts.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+
+    const rss = `<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom">
+    <channel>
+        <title>ㅂㄹㄱ</title>
+        <link>https://blog.hyochan.site</link>
+        <description>개발자의 일상을 담은 블로그</description>
+        <language>ko-kr</language>
+        <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+        <atom:link href="https://blog.hyochan.site/rss.xml" rel="self" type="application/rss+xml" />
+        ${sortedPosts.map(post => `
+        <item>
+            <title><![CDATA[${post.title}]]></title>
+            <link>https://blog.hyochan.site${post.permalink}</link>
+            <guid>https://blog.hyochan.site${post.permalink}</guid>
+            <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+            <description><![CDATA[${post.description || ''}]]></description>
+        </item>`).join('')}
+    </channel>
+</rss>`;
+
+    return new Response(rss, {
+        headers: {
+            'Content-Type': 'application/xml',
+            'Cache-Control': 'max-age=0, s-maxage=3600'
+        }
+    });
 }
--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,3 +1,51 @@
+import { getPosts } from "$lib/utils.server";
+
+export const prerender = true;
+
 export const GET = async () => {
-    return new Response("A");
+    const posts = getPosts();
+    const sortedPosts = posts.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://blog.hyochan.site</loc>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>https://blog.hyochan.site/about</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>https://blog.hyochan.site/category</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>https://blog.hyochan.site/tags</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>https://blog.hyochan.site/search</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.5</priority>
+    </url>
+    ${sortedPosts.map(post => `
+    <url>
+        <loc>https://blog.hyochan.site${post.permalink}</loc>
+        <lastmod>${new Date(post.updated || post.date).toISOString()}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.6</priority>
+    </url>`).join('')}
+</urlset>`;
+
+    return new Response(sitemap, {
+        headers: {
+            'Content-Type': 'application/xml',
+            'Cache-Control': 'max-age=0, s-maxage=3600'
+        }
+    });
 }
