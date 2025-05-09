022aefc4b57b9e86bc6b3b3b18a9dabca66304f9
장효찬 <jang@hyochan.site>
2025-05-09 10:56:36 +0900

    RSS 피드 URL 동적 처리 및 정렬 개선
    - `src/routes/rss.xml/+server.ts` 파일 수정: GET 함수에 event 인자 추가
    - RSS 피드의 atom:link href를 동적으로 변경하여 URL 처리 개선
    - 포스트의 guid 속성을 isPermaLink="false"로 수정하여 퍼머링크 처리 개선
    - 포스트 링크를 encodeURI로 감싸서 URL 인코딩 처리 추가

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -2,9 +2,10 @@ import { getPosts } from "$lib/utils.server";
 
 export const prerender = true;
 
-export const GET = async () => {
+export const GET = async (event) => {
     const posts = getPosts();
     const sortedPosts = posts.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+    const { url } = event;
 
     const rss = `<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom">
@@ -14,12 +15,12 @@ export const GET = async () => {
         <description>개발자의 일상을 담은 블로그</description>
         <language>ko-kr</language>
         <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
-        <atom:link href="https://blog.hyochan.site/rss.xml" rel="self" type="application/rss+xml" />
+        <atom:link href="${url.origin}/rss.xml" rel="self" type="application/rss+xml" />
         ${sortedPosts.map(post => `
         <item>
             <title><![CDATA[${post.title}]]></title>
-            <link>https://blog.hyochan.site${post.permalink}</link>
-            <guid>https://blog.hyochan.site${post.permalink}</guid>
+            <link>https://blog.hyochan.site${encodeURI(post.permalink)}</link>
+            <guid isPermaLink="false">${post.permalink}</guid>
             <pubDate>${new Date(post.date).toUTCString()}</pubDate>
             <description><![CDATA[${post.description || ''}]]></description>
         </item>`).join('')}
