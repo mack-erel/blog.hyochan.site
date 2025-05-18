ce9fd79e7eb364ad22ade112a1117b9439d0b3fc
Henry Jang <jang@hyochan.site>
2025-05-18 16:35:56 +0900

    포스트 콘텐츠 처리 및 RSS 피드 개선
    - `src/routes/rss.xml/+server.ts` 파일 수정: 포스트 콘텐츠를 마크다운으로 파싱하고 이미지 URL 리사이징 처리 추가
    - 처리된 콘텐츠를 RSS 피드에 포함하도록 수정하여 출력 형식 개선

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -7,6 +7,23 @@ export const GET = async (event) => {
     const posts = getPosts();
     const sortedPosts = posts.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 
+    // 모든 포스트 콘텐츠 처리를 위한 Promise 배열
+    const processedPosts = await Promise.all(sortedPosts.map(async post => {
+        // 마크다운 파싱
+        let parsedContent = await marked.parse(post.content || '');
+        
+        // 이미지 URL 리사이징 처리
+        parsedContent = parsedContent.replace(
+            /https:\/\/blog-files\.hyochan\.site\/(.+?)\.(png|jpg|jpeg|gif|webp)/gi,
+            "https://blog-files.hyochan.site/cdn-cgi/image/width=512,quality=50,format=webp/$1.$2"
+        );
+        
+        return {
+            ...post,
+            parsedContent
+        };
+    }));
+
     const rss = `<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom">
     <channel>
@@ -16,14 +33,14 @@ export const GET = async (event) => {
         <language>ko-kr</language>
         <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
         <atom:link href="https://blog.hyochan.site/rss.xml" rel="self" type="application/rss+xml" />
-        ${sortedPosts.map(post => `
+        ${processedPosts.map(post => `
         <item>
             <title><![CDATA[${post.title}]]></title>
             <link>https://blog.hyochan.site${encodeURI(post.permalink)}</link>
             <guid isPermaLink="false">${post.permalink}</guid>
             <pubDate>${new Date(post.date).toUTCString()}</pubDate>
             <description><![CDATA[${post.description || ''}]]></description>
-            <content:encoded><![CDATA[${marked.parse(post.content || '')}]]></content:encoded>
+            <content:encoded><![CDATA[${post.parsedContent}]]></content:encoded>
         </item>`).join('')}
     </channel>
 </rss>`;
